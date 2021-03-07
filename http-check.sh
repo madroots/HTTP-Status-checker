df0bb7eb314c09065d2a31fac74b839715d979a9
madroots <77987178+madroots@users.noreply.github.com>
2021-03-07 13:15:28 +0100

    Update http-check.sh

--- http-check.sh
+++ http-check.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ###########################################################################
-#Script Name	:website_check_status.sh
-#Description	:Script to check HTTP status of a service. (load from file)
+#Script Name	  :http-check.sh
+#Description	  :Script to check HTTP status of a service. (load from file)
 #Args           :No args
 #Author       	:Sim0n
 #Email         	:simon.barker@linuxmail.org
 ###########################################################################
 #
-#
+# Header ASCII Art - BEGIN
 echo
 clear
 printf "\e[1;92m    __    __  __                   __              __  \e[0m\n"
@@ -17,7 +17,9 @@ printf "\e[1;92m / / / / /_/ /_/ /_/ /_____/ /__/ / / /  __/ /__/ ,<   \e[0m\n"
 printf "\e[1;92m/_/ /_/\__/\__/ .___/      \___/_/ /_/\___/\___/_/|_|  \e[0m\n"
 printf "\e[1;92m             /_/                  v1.2 made by Sim0n   \e[0m\n"
 #
+# Header ASCII Art - END
 #
+# Loader - BEGIN
 echo -e "\e[93m Loading.\e[5m.\e[0m"
 sleep 3 &
 PID=$!
@@ -29,41 +31,47 @@ do
   printf "\b${sp:i++%${#sp}:1}"
 done
 clear
+# Loader - END
 #
-#
+# Check 1st group of websites - BEGIN
 echo
-echo -e "\e[93m Nginx HTTP beeSport check \e[0m"
-conf=/home/user/Desktop/bin/servers.conf
+echo -e "\e[93m Check 1st group of websites \e[0m"      # Title - Change according to your group name
+conf=servers.conf                                       # Config file - default: server.conf
 while IFS= read -r srv; do
-if  [[ $srv == _* ]]  ;
+if  [[ $srv == _* ]]  ;                                 # Identifier character - Character "_" identifies 1st group of websites to check
 then
 srv=${srv:1}
 curl -Is $srv | head -1 && echo -e "\e[37m$srv\e[0m"
 fi
 done < "$conf"
+# Check 1st group of websites - END
+#
+# Check 2nd group of websites - BEGIN
 echo
-echo -e "\e[93m Nginx HTTP other check \e[0m"
-#
-#
-conf=/home/user/Desktop/bin/servers.conf
+echo -e "\e[93m Check 2nd group of websites \e[0m"      # Title - Change according to your group name
+conf=servers.conf                                       # Config file - default: server.conf
 while IFS= read -r srv; do
-if  [[ $srv == +* ]]  ;
+if  [[ $srv == +* ]]  ;                                 # Identifier character - Character "+" identifies 2nd group of websites to check
 then
 srv=${srv:1}
 curl -Is $srv | head -1 && echo -e "\e[37m$srv\e[0m"
 fi
 done < "$conf"
+# Check 2nd group of websites - END
 #
-#
+# Check 3rd group of websites - BEGIN
 echo
-echo -e "\e[93m Production beeSport HTTP check \e[0m"
-#
-#
-conf=/home/user/Desktop/bin/servers.conf
+echo -e "\e[93m Check 3rd group of websites \e[0m"      # Title - Change according to your group name
+conf=servers.conf                                       # Config file - default: server.conf
 while IFS= read -r srv; do
-if  [[ $srv == -* ]]  ;
+if  [[ $srv == -* ]]  ;                                 # Identifier character - Character "-" identifies 3rd group of websites to check
 then
 srv=${srv:1}
 curl -Is $srv | head -1 && echo -e "\e[37m$srv\e[0m"
 fi
 done < "$conf"
+# Check 3rd group of websites - END
+#
+#
+## In case you want to add new group of websites, Copy one of the blocks above, change Title and set Identifier character of your liking. 
+## Then go to servers.conf and add your websites and prefix then with Identifier character you have choosen.
